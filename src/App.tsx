f35391ec6447d7f04070a021b22bd36e5fd71bf6
muhamash <ashraful@xpeedlab.tech>
2024-01-13 02:16:36 +0600

    todays done

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ function App() {
     countries: [] as Country[],
     selectedCountries: [] as any[],
     searchResults: [] as any[],
+    loading: true,
+    error: null as string | null,
   });
 
   // console.log( state.selectedCountries, `type of state ${typeof state.selectedCountries}`, Array.isArray(state.selectedCountries) )
@@ -30,12 +32,13 @@ function App() {
         const response = await fetch( 'https://restcountries.com/v3.1/all' );
         const data = await response.json();
 
-        setState( ( prev ) => ( { ...prev, countries: data } ) );
+        setState( ( prev ) => ( { ...prev, countries: data, loading: false  } ) );
 
       }
       catch ( error )
       {
         console.error( 'Error fetching data:', error );
+        setState((prev) => ({ ...prev, error: 'Error fetching data', loading: false }));
       }
     };
 
@@ -66,19 +69,25 @@ function App() {
       <div className='py-5'>
         <SearchBar onSearch={handleSearch} />
       </div>
-      <Counter counter={state.selectedCountries.length} />
-      <div className=''>
-        <Countries
-          onAdd={incrementCounter}
-          countries={
-            state.searchResults.length > 0
-              ? state.searchResults
-              : state.countries
-          }
-        />
-      </div>
+      {state.loading && <p className='text-xl text-yellow-600 font-mono'>Loading...</p>}
+      {state.error && <p>Error: {state.error}</p>}
+      {!state.loading && !state.error && (
+        <>
+          <Counter counter={state.selectedCountries.length} />
+          <div className=''>
+            <Countries
+              onAdd={incrementCounter}
+              countries={
+                state.searchResults.length > 0
+                  ? state.searchResults
+                  : state.countries
+              }
+            />
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
